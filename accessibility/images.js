4e99d5a71bbbb5798ce62e4f2ad056c724f0465b
Mikael Brevik <mikaelbre@gmail.com>
2020-09-13 19:30:10 +0200

    initial

---
+++ accessibility/images.js
@@ -0,0 +1,6 @@
+export { default as bg } from './assets/variant-bg.png';
+export { default as difi } from './assets/difi.png';
+export { default as difi2 } from './assets/difi2.png';
+export { default as arngren } from './assets/arngren.png';
+export { default as webaim } from './assets/webaim.png';
+export { default as keyboard } from './assets/keyboard.jpg';
