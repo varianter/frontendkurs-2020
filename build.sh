0d9cfa71182fb3d4a04ed2101260360fd7d8000a
Mikael Brevik <mikaelbre@gmail.com>
2020-09-15 08:33:20 +0200

    Only deploy hooks presentation

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
+rm -rf build/
 mkdir -p build/
 
-npx mdx-deck build ./accessibility/deck.mdx
-mv ./public ./build/accessibility
-
-npx mdx-deck build ./react-typescript/deck.mdx
-mv ./public ./build/react-typescript
-
 npx mdx-deck build ./react-hooks/deck.mdx
-mv ./public ./build/react-hooks
-
-
-cp ./index.html ./build/
+mv ./public ./build
